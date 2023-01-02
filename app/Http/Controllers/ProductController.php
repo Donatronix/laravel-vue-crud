314301c5f7d39b84109340de475f3e220a6101d0
Donald Blessing <donaldblessing9@gmail.com>
2023-01-02 21:01:34 +0100

    Npm update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = ProductResource::collection(Product::orderBy('created_at')->get());
+        return response()->json($products);
+    }
+    public function store(Request $request)
+    {
+        $product = Product::create([
+            'name' => $request->input('name'),
+            'detail' => $request->input('detail')
+        ]);
+        return response()->json('Product created!');
+    }
+    public function show($id)
+    {
+        $product = new ProductResource(Product::find($id));
+        return response()->json($product);
+    }
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json('Product updated!');
+    }
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product deleted!');
+    }
 }
